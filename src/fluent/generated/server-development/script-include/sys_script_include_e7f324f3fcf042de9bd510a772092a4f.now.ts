5269b9a7e790afc85980f6ad32cd7530927b3d27
umang-it <72683017+umang-it@users.noreply.github.com>
2026-09-12 15:03:28 +0530

    Update sys_script_include_e7f324f3fcf042de9bd510a772092a4f.now.ts

--- src/fluent/generated/server-development/script-include/sys_script_include_e7f324f3fcf042de9bd510a772092a4f.now.ts
+++ src/fluent/generated/server-development/script-include/sys_script_include_e7f324f3fcf042de9bd510a772092a4f.now.ts
@@ -4,7 +4,7 @@ ScriptInclude({
     $id: Now.ID['e7f324f3fcf042de9bd510a772092a4f'],
     name: 'miracle',
     script: Now.include('./sys_script_include_e7f324f3fcf042de9bd510a772092a4f.server.js'),
-    description: 'Reusable server-side logic for the popular application umang.',
+    description: 'Reusable server-side logic for the popular application umang verma.',
     apiName: 'x_popular.miracle',
     clientCallable: false,
     mobileCallable: false,
